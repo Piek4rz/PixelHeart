c3a69e7ed32832dbd482c0d907f045b1689056a0
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2023-06-06 05:39:31 +0200

    mowisz masz

--- Web/src/Components/Main/Chat.jsx
+++ Web/src/Components/Main/Chat.jsx
@@ -3,19 +3,28 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const Chat = ({ token, loggedUser, chatUser }) => {
+const Chat = ({ token, loggedUser, userChatId }) => {
   const [newMasseg, setMessage] = useState({
     id: 0,
     userId: 0,
     timestamp: "",
     message: "",
   });
+  const [userChatting, setUserChatting] = useState({
+    id: 0,
+    name: "",
+    email: "",
+    password: "",
+    age: 0,
+    photo: "",
+    backstory: "",
+  });
   const [allMessages, setAllMessages] = useState([]);
   useEffect(() => {
-    if (loggedUser && chatUser) {
+    if (loggedUser && userChatId) {
       axios
         .get(
-          `https://localhost:7081/api/User/${loggedUser.id}/mmessage/${chatUser.id}`
+          `https://localhost:7081/api/User/${loggedUser.id}/mmessage/${userChatId}`
         )
         .then((res) => {
           var data = res.data;
@@ -25,14 +34,66 @@ const Chat = ({ token, loggedUser, chatUser }) => {
         .catch((error) => {
           console.log(error);
         });
+      var message = {
+        id: allMessages.length,
+        userId: loggedUser.id,
+        timestamp: "",
+        message: "",
+      };
+      setMessage(message);
+      axios.get(`https://localhost:7081/api/User/${userChatId}`).then((res) => {
+        var data = res.data;
+        setUserChatting(data);
+      });
     }
-  }, []);
+  }, [userChatId]);
+  const reloadMessage = () => {
+    axios
+      .get(
+        `https://localhost:7081/api/User/${loggedUser.id}/mmessage/${userChatId}`
+      )
+      .then((res) => {
+        var data = res.data;
+        setAllMessages(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleWyslij = () => {
+    if (newMasseg.message != "") {
+      if (allMessages.length === 0) {
+        setAllMessages([newMasseg]);
+      } else {
+        setAllMessages([...allMessages, newMasseg]);
+      }
+      axios
+        .put(
+          `https://localhost:7081/api/User/${loggedUser.id}/mmessage/${userChatId}`,
+          setAllMessages
+        )
+        .catch((error) => console.log(error.message));
+      reloadMessage();
+    }
+  };
+  const handleMessageChange = ({ currentTarget: input }) => {
+    setMessage({ ...newMasseg, ["message"]: input.value });
+  };
   return (
     <>
       <div>
-        {allMessages.map((mes) => (
-          <h2>mess.userId - mes.message</h2>
-        ))}
+        <h1>Chat z {userChatting.name}</h1>
+        <div>
+          {allMessages &&
+            allMessages.map((mes) => (
+              <h2>
+                {mes.userId} - {mes.message}
+              </h2>
+            ))}
+        </div>
+        <input onChange={handleMessageChange} value={newMasseg.message}></input>
+        <button onClick={handleWyslij}>Wyslij</button>
       </div>
     </>
   );
--- Web/src/Pages/Main.jsx
+++ Web/src/Pages/Main.jsx
@@ -6,11 +6,16 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import FindLove from "../Components/Main/FindLove";
+<<<<<<< Updated upstream
 import "../Components/Background/MainBackground.css";
 import Navbar from "../Components/Navbar/Navbar";
+=======
+import Chat from "../Components/Main/Chat";
+>>>>>>> Stashed changes
 
 const Main = () => {
   const [showFindLove, setShowFindLove] = useState(false);
+  const [showChat, setSchowChat] = useState(false);
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [loggedUser, setLoggedUser] = useState({
     id: 0,
@@ -75,7 +80,12 @@ const Main = () => {
         });
     }
   };
-  const handleChat = () => {};
+  const [userChatId, setUserChatId] = useState(0);
+  const handleChat = (id) => {
+    setUserChatId(id);
+    setShowFindLove(false);
+    setSchowChat(true);
+  };
   const handleProfile = () => {};
 
   const handleShowFindLove = () => {
@@ -104,6 +114,7 @@ const Main = () => {
     <div className="mainContainer">
       <div className="mainContents">
         <h1>Witaj w mojej karczmie</h1>
+<<<<<<< Updated upstream
         <Navbar loggedUser={loggedUser} matches={matches} handleChat={handleChat} handleProfile={handleProfile} handleLogOut={handleLogOut}>
 
         </Navbar>
@@ -121,6 +132,42 @@ const Main = () => {
         )}
       </div>
 
+=======
+        <img
+          src="https://generated.inspirobot.me/a/5y6LqaADeW.jpg"
+          alt="InspiroBot"
+        />
+        <h1>{loggedUser.name}</h1>
+        <h1>Lvl.{loggedUser.age}</h1>
+        <button onClick={handleProfile}>Profil</button>
+        <button onClick={handleShowFindLove}>Zdobywaj</button>
+        <h2>Lista zdobyczy</h2>
+        <ul>
+          {matches.map((match) => (
+            <button onClick={() => handleChat(match.id)} name={match.id}>
+              {match.name}
+            </button>
+          ))}
+        </ul>
+        <button onClick={handleLogOut}>Wyloguj</button>
+      </nav>
+      {showFindLove && (
+        <div style={{ flex: 1 }}>
+          <FindLove
+            handleFindLove={handleFindLove}
+            token={token}
+            findLove={findLove}
+            loggedUser={loggedUser}
+            reloadZdobycze={reloadZdobycze}
+          />
+        </div>
+      )}
+      {showChat && (
+        <div style={{ flex: 1 }}>
+          <Chat token={token} loggedUser={loggedUser} userChatId={userChatId} />
+        </div>
+      )}
+>>>>>>> Stashed changes
     </div>
   );
 };
