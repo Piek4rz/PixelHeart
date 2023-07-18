cb0efce292736c84a19821ee3e3db73b51d6641d
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2023-07-18 23:16:51 +0200

    routy zrobione good

--- Web/src/App.js
+++ Web/src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 
 import "./Components/Button/Button.css";
 import "./Components/Background/HomeBackground.css";
-<<<<<<< Updated upstream
-import { Route, Routes, Navigate } from "react-router";
-=======
 import { Route, Routes } from "react-router";
->>>>>>> Stashed changes
 import Home from "./Pages/Home";
 import Registration from "./Pages/Registration";
 import Login from "./Pages/Login";
@@ -14,24 +10,6 @@ import CreateProfile from "./Pages/CreateProfile";
 import Main from "./Pages/Main";
 
 const App = () => {
-<<<<<<< Updated upstream
-  const userLoggedIn = localStorage.getItem("token") != null;
-  return (
-    <Routes>
-      {userLoggedIn ? (
-        <>
-          <Route path="/" element={<Main />} />
-        </>
-      ) : (
-        <>
-          <Route path="/" element={<Navigate replace to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/register" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/CreateProfile" element={<CreateProfile />} />
-        </>
-      )}
-=======
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -39,7 +17,6 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/CreateProfile" element={<CreateProfile />} />
       <Route path="/main/*" element={<Main />} />
->>>>>>> Stashed changes
     </Routes>
   );
 };
